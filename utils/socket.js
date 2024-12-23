76c85ffaa2017fc75dfb2ede9379a0fc6c25fb09
KareemAbbas1 <82244782+KareemAbbas1@users.noreply.github.com>
2024-12-23 21:38:26 +0200

    Debug 12

--- utils/socket.js
+++ utils/socket.js
@@ -6,18 +6,19 @@ const initializeSocket = (io) => {
         console.log('A user connected with id: ' + socket.id);
 
         // Join a specific conversation
-        socket.on('joinConversation', (conversationId) => {
+        socket.on('joinConversation', (payload) => {
 
-            const parsedId = conversationId.conversation_id;
-            console.log("Conversation Id", conversationId)
+            const conversationId = payload.conversationId;
+            console.log("Conversation Id", payload)
             socket.join(parsedId);
             console.log(`User joined conversation: ${parsedId}`)
         });
 
         // Send a new message
         socket.on('sendMessage', async (message) => {
-            const parsedMessage = message;
-            console.log("New message send", parsedMessage.messageContent);
+            const parsedMessage = await JSON.parse(message);
+            console.log("New message send", parsedMessage);
+
 
             // Validate conversationId
             if (!parsedMessage.conversationId || typeof parsedMessage.conversationId !== 'string' || parsedMessage.conversationId.trim() === '') {
@@ -25,37 +26,35 @@ const initializeSocket = (io) => {
                 return;
             }
 
-            try {
-                // Save message to Firestore
-                const newMessageRef = await db.collection('conversations')
-                    .doc(parsedMessage.conversationId)
-                    .collection('messages')
-                    .add({
-                        conversation_id: parsedMessage.conversationId,
-                        sender: parsedMessage.sender,
-                        message_content: parsedMessage.messageContent,
-                        attachments: parsedMessage.attachments,
-                        read: false,
-                        end_session: parsedMessage.endSession || false,
-                        timestamp: admin.firestore.FieldValue.serverTimestamp(),
-                    });
 
-                // Update the last message in the conversation
-                await db.collection('conversations').doc(parsedMessage.conversationId).update({
-                    last_message: parsedMessage.messageContent,
-                    last_message_timestamp: admin.firestore.FieldValue.serverTimestamp()
+            // Save message to Firestore
+            const newMessageRef = await db.collection('conversations')
+                .doc(parsedMessage.conversationId)
+                .collection('messages')
+                .add({
+                    conversation_id: parsedMessage.conversationId,
+                    sender: parsedMessage.sender,
+                    message_content: parsedMessage.messageContent,
+                    attachments: parsedMessage.attachments,
+                    read: false,
+                    end_session: parsedMessage.endSession || false,
+                    timestamp: admin.firestore.FieldValue.serverTimestamp(),
                 });
 
-                // Emit the new message to the conversation room
-                const newMessageSnapshot = await newMessageRef.get();
-                const newMessage = newMessageSnapshot.data();
-                io.to(parsedMessage.conversationId).emit('newMessage', {
-                    id: newMessageRef.id,
-                    ...newMessage,
-                });
-            } catch (error) {
-                console.error('Error sending message:', error);
-            }
+            // Update the last message in the conversation
+            await db.collection('conversations').doc(parsedMessage.conversationId).update({
+                last_message: parsedMessage.messageContent,
+                last_message_timestamp: admin.firestore.FieldValue.serverTimestamp()
+            });
+
+
+            // Emit the new message to the conversation room
+            const newMessageSnapshot = await newMessageRef.get();
+            const newMessage = newMessageSnapshot.data();
+            io.to(parsedMessage.conversationId).emit('newMessage', {
+                id: newMessageRef.id,
+                ...newMessage,
+            });
         });
 
         socket.on('newMessage', async (message) => {
